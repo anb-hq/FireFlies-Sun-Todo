df7b006325a0cf6b105aca7164f16b7e8d3742bf
RelexSun <relexsunnop@gmail.com>
2023-08-31 09:40:58 +0700

    interactive form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
+
 export default function Form() {
+  const [title, setTitle] = useState("");
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    if(!title) return;
+    setTitle("")
+    console.log(title)
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit}>
       <div className="">
-      <input type="text" placeholder="Todo list" />
+      <input type="text" 
+      placeholder="Todo list" 
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      
+      />
       <button>
         <AiOutlinePlus />
       </button>
